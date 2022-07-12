2ff54641e3dd88cb7bd35d981ffc76e98dccc30d
Ayrton <ayrtonrafael@gmail.com>
2022-07-12 18:02:55 -0300

    cidadaoDePrimeira

---
+++ funcao/cidadaoDePrimeiraClasse.js
@@ -0,0 +1,34 @@
+// função em JS é First-Class Objetct 
+// High-order function
+
+//criar de forma literal
+function fun1() { }
+
+//armazenar em uma variável
+const fun2 = function () { }
+
+//armanezar em um array
+const array = [function (a,b) {return a+b}, fun1, fun2]
+console.log(array[0](2,3))
+
+//armazenar em um atributo de objeto 
+const obj = {}
+obj.falar = function () { return 'Opa'}
+console.log(obj.falar())
+
+//Passar função como param
+function run(fun) {
+    fun()
+}
+
+run ( function () {console.log('Execuntando...')})
+
+//Uma função pode retornar/conter uma função
+function soma(a, b) {
+    return function (c) {
+        console.log(a + b + c)
+    }
+
+}
+
+soma(2,3)(4)
