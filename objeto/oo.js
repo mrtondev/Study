2b7f8066b3ea905aa7344f99b3873baa362a0a89
Ayrton Souza <cachorroquentecmostrada@gmail.com>
2022-09-23 22:15:36 -0300

    O O

---
+++ objeto/oo.js
@@ -0,0 +1,18 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+//PROCEDURAL
+
+Processamento(valor1, valor2, valor3)
+
+// OO 
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    Processamento(){
+        //...
+    }
+}
+
+objeto.Processamento() //FOCO PASSOU A SER O OBJETO
