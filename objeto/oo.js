7d30a16f0dd1c2b587c4e56310840d39517061d2
Ayrton Souza <cachorroquentecmostrada@gmail.com>
2022-09-24 03:32:40 -0300

    Revisão Objeto

--- objeto/oo.js
+++ objeto/oo.js
@@ -16,3 +16,9 @@ objeto = {
 }
 
 objeto.Processamento() //FOCO PASSOU A SER O OBJETO
+
+// Principios importantes:
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herança (prototype)
+// 4. Polimorfismo
